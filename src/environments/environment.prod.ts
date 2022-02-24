fe1bb3724bd6c8aa9bbeb82dd895e40e4ed79c2e
Ahmed Mohammed Yousry <ahmedmohammedyousry33@gmail.com>
2022-02-24 10:28:58 +0200

    modify server js and add production env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  api_URL: 'https://lit-atoll-09640.herokuapp.com/api',
+  pusher: {
+    key: '473d6c2ef580e2c7c5d8',
+    // key: 'b8dd19d436b6a0402060',
+  },
+
+  storage_URL: 'https://lit-atoll-09640.herokuapp.com/storage'
 };
