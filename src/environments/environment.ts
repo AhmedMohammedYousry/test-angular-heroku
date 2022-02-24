3f4e77e7456a357b38fc9473df81d709fbde8a04
Ahmed Mohammed Yousry <ahmedmohammedyousry33@gmail.com>
2022-02-24 10:13:29 +0200

    first commit on angular for heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  api_URL: 'http://localhost:8000/api',
+  api_URL: 'https://lit-atoll-09640.herokuapp.com/api',
 
   pusher: {
     key: '473d6c2ef580e2c7c5d8',
     // key: 'b8dd19d436b6a0402060',
   },
-  
-  storage_URL: 'http://127.0.0.1:8000/storage'
+
+  storage_URL: 'https://lit-atoll-09640.herokuapp.com/storage'
 };
 
 /*
